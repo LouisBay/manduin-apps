352cd7194809b5949bbe3a75506149e4d33397c2
luthfiakhakim <khakimluthfi95@gmail.com>
2022-05-14 19:11:26 +0700

    Forgot & Reset Password Layout
    Add forgot password, reset password success, overwrite vector forgot pass, re-edit login register layout, string

---
+++ app/src/main/java/com/bangkit/manduin/ResetPasswordSuccessActivity.kt
@@ -0,0 +1,11 @@
+package com.bangkit.manduin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResetPasswordSuccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password_success)
+    }
+}
